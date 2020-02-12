8b18b1fab04057793bebbb92050efea3e28775f8
Yalgie <dan@thecodingforge.com.au>
2020-02-12 14:46:40 +1000

    basic start.spring.io app

---
+++ src/test/java/com/example/rest/rest/app/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.rest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
